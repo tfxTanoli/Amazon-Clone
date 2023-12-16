88f9f1234938d44a3119d30a5fe86ebe120697a8
tfxTanoli <usman5194999@gmail.com>
2023-12-16 09:31:25 +0500

    tenth commit

--- Frontend/src/Components/AdminPanel.js
+++ Frontend/src/Components/AdminPanel.js
@@ -1,19 +1,23 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import AdminNavbar from './AdminNavbar';
 import Additems from './Additems';
 import DeleteItem from './DeleteItem';
 import UpdateItem from './UpdateItem';
 import ViewItem from './ViewItem';
+import AdminSidebar from './AdminSidebar';
 
 function AdminPanel() {
   const [activePage, setActivePage] = useState('add');
 
-  const sidebarStyle = {
-    backgroundColor: '#e6e6fa', // Light lavender
-    color: '#1f3a93', // Dark navy blue
+  const sidebarStyle1 = {
+    backgroundColor: '#1f1e3e', // Dark lavender
+    color: '#fff', // White text
     padding: '20px',
     height: '100vh',
-    borderRadius: '15px', // Rounded corners for softness
+    borderRadius: '15px',
+    position: 'sticky',
+    top: '0',
   };
 
   const buttonStyle = {
@@ -28,11 +32,13 @@ function AdminPanel() {
     textAlign: 'left',
     cursor: 'pointer',
     transition: 'background-color 0.3s',
-    borderRadius: '5px', // Rounded corners for buttons
+    borderRadius: '5px',
   };
 
   const sidebarHoverStyle = {
-    backgroundColor: '#f5e5db', // Lighter lavender on hover for subtle highlight
+    backgroundColor: '#3f51b5', // Indigo on hover
+    color: '#fff', // White text on hover
+    width: '100%', // Full width on hover
   };
 
   const changePage = (page) => {
@@ -54,83 +60,48 @@ function AdminPanel() {
     }
   };
 
+  const handleItemClick = (page) => {
+    setActivePage(page);
+  };
+
   return (
-    <div className="container-fluid">
-      <nav
-        className="navbar navbar-expand-lg navbar-dark bg-dark"
-        style={{
-          display: 'flex',
-          justifyContent: 'space-evenly',
-          alignItems: 'center',
-          borderRadius: '15px', // Rounded corners for softness
-        }}
-      >
-        <img src="admin.png" style={{ paddingLeft: '25px' }} alt="Admin Logo" />
-        <Link
-          to="/Home"
-          className="navbar-brand"
-          style={{ color: '#fbf0e0' }} // Light peach for text
-        >
-          Administrator
-        </Link>
-        <Link
-          to="/landingpage"
-          className="navbar-brand"
-          style={{ color: '#fbf0e0' }}
-        >
-          LandingPage
-        </Link>
-        <Link
-          to="/products"
-          className="navbar-brand"
-          style={{ color: '#fbf0e0' }}
-        >
-          Show All Products
-        </Link>
-      </nav>
-      <div className="row" style={{ height: '100vh', marginTop: '5px' }}>
-        <div className="col-md-3">
-          <div className="sidebar" style={sidebarStyle}>
-            <button
-              style={{
-                ...buttonStyle,
-                ...(activePage === 'add' && sidebarHoverStyle),
-              }}
-              onClick={() => changePage('add')}
-            >
-              <i className="fas fa-plus-circle"></i> Add Item
-            </button>
-            <button
-              style={{
-                ...buttonStyle,
-                ...(activePage === 'delete' && sidebarHoverStyle),
-              }}
-              onClick={() => changePage('delete')}
-            >
-              <i className="fas fa-trash-alt"></i> Delete Item
-            </button>
-            <button
-              style={{
-                ...buttonStyle,
-                ...(activePage === 'update' && sidebarHoverStyle),
-              }}
-              onClick={() => changePage('update')}
-            >
-              <i className="fas fa-edit"></i> Update Item
-            </button>
-            <button
-              style={{
-                ...buttonStyle,
-                ...(activePage === 'view' && sidebarHoverStyle),
-              }}
-              onClick={() => changePage('view')}
-            >
-              <i className="fas fa-eye"></i> View Item
-            </button>
+    <div className="container-fluid" style={{ backgroundColor: 'rgba(37, 32, 64, 255)' }}>
+      <AdminNavbar />
+      <div className="row" style={{ marginTop: '5px' }}>
+        <div className="container-fluid position-sticky">
+          <div className="sidebar-menu d-flex position-sticky">
+            <div className="col-md-2 sidebar" style={sidebarStyle1}>
+              <ul>
+                <li onClick={() => handleItemClick('dashboard')} style={{ ...buttonStyle, ...(activePage === 'dashboard' && sidebarHoverStyle) }}>
+                  <i className="bi bi-house-fill"></i> Dashboard
+                </li>
+                <li onClick={() => handleItemClick('catalog')} style={{ ...buttonStyle, ...(activePage === 'catalog' && sidebarHoverStyle) }}>
+                  <i className="bi bi-box"></i> Catalog
+                </li>
+                <li onClick={() => handleItemClick('add')} style={{ ...buttonStyle, ...(activePage === 'add' && sidebarHoverStyle) }}>
+                  <i className="bi bi-plus-square"></i> Add Product
+                </li>
+                <li onClick={() => handleItemClick('view')} style={{ ...buttonStyle, ...(activePage === 'view' && sidebarHoverStyle) }}>
+                  <i className="bi bi-eye"></i> View Product
+                </li>
+                <li onClick={() => handleItemClick('update')} style={{ ...buttonStyle, ...(activePage === 'update' && sidebarHoverStyle) }}>
+                  <i className="bi bi-pencil"></i> Update Product
+                </li>
+                <li onClick={() => handleItemClick('delete')} style={{ ...buttonStyle, ...(activePage === 'delete' && sidebarHoverStyle) }}>
+                  <i className="bi bi-trash"></i> Delete Product
+                </li>
+                <li onClick={() => handleItemClick('notifications')} style={{ ...buttonStyle, ...(activePage === 'notifications' && sidebarHoverStyle) }}>
+                  <i className="bi bi-bell-fill"></i> Notifications
+                </li>
+                <li onClick={() => handleItemClick('profile')} style={{ ...buttonStyle, ...(activePage === 'profile' && sidebarHoverStyle) }}>
+                  <i className="bi bi-person-fill"></i> Profile
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-8 main-content" style={{ width: '1500px', overflow: 'hidden', backgroundColor: '#423875', color: 'white', overflow: 'scroll' }}>
+              {renderActivePage()}
+            </div>
           </div>
-        </div>
-        <div className="col-md-9" style={{maxHeight:"70vh",overflowY:"scroll"}}>
-          {renderActivePage()} {/* Render the active page */}
         </div>
       </div>
     </div>
