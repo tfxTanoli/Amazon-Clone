93c47c8ee21480c69e237043a87f04e1a7ac716b
tfxTanoli <usman5194999@gmail.com>
2023-12-22 12:07:16 +0500

    index.js update

---
+++ Backend/index.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import express from 'express';
+import cors from 'cors';
+import bodyParser from "body-parser";
+import adminRouter from './routes/cruds.js';
+import userRouter from './routes/users.js';
+import loginRouter from './routes/sign-in.js';
+
+
+
+const app = express();
+
+const url = "mongodb+srv://usman:tfxUsman124@cluster0.sroacgg.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(url).then(() => {
+    console.log("Connected to the database");
+    app.listen(5000, () => {
+      console.log("Server is running on port 5000");
+    });
+  });
+
+  app.use(cors());
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/admin", adminRouter);
+app.use("/", router);
+app.get("/sign-in", loginUser);
+app.post("/signup", addUser);
