af8a050e54daaf6c87141895ced3ca4f5f39ec94
tfxTanoli <usman5194999@gmail.com>
2023-12-27 20:10:22 +0500

    backend deleted

--- Backend/routes/sign-in.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { loginUser } from "../controllers/sign-in.js";
-
-const router = express.Router();
-
-router.get("/sign-in",loginUser);
-
-export default router;
